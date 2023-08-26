72d47e3d7134c2911c9a936bec27f0e306375a59
Abel George Antony <abelgeorgeantony@gmail.com>
2023-08-26 14:07:11 +0530

    Version 1 commit.HAS ERRORS

---
+++ typingtest.js
@@ -0,0 +1,61 @@
+const texttotype = document.getElementById("texttotype");
+const textinput = document.getElementById("testspace");
+
+let key;
+
+
+textinput.onkeydown = function(event) {
+    key = event.keyCode || event.charCode;
+
+};
+
+
+function evaluatetyping() {
+    const enteredtext = textinput.value;
+    const textlength = enteredtext.length;
+    if( key === 8 || key === 46 ) {
+        return 0;
+    }
+    else {
+        if(texttotype.innerText.length < textlength) {
+            restricttyping();
+            return 0;
+        }
+    
+        if(issamechar(enteredtext,textlength)) {
+            console.log("CORRECT");
+            indicatecorrect();
+        }
+        else {
+            indicatewrong();
+            console.log("INnnnnnnnnnnCORRECT");
+        }
+    }
+}
+
+function restricttyping() {
+    textinput.value = textinput.value.slice(0, -1);
+}
+
+
+let colorprint = texttotype.innerHTML;
+// texttotype.innerText.substr(0, (textinput.value.length-1))
+function indicatecorrect() {
+    if(colorprint.lastIndexOf("</span") === -1) {
+        texttotype.innerHTML = colorprint.substr(0,(textinput.value.length-1))+"<span style='background-color:green'>"+textinput.value.substr((textinput.value.length-1),(textinput.value.length))+"</span>"+texttotype.innerText.substr((textinput.value.length));
+    }
+    else {
+        texttotype.innerHTML = colorprint.substr(0,(colorprint.lastIndexOf("</span>")+6))+"<span style='background-color:green'>"+textinput.value.substr((textinput.value.length-1),(textinput.value.length))+"</span>"+texttotype.innerText.substr((textinput.value.length));
+    }
+    colorprint = texttotype.innerHTML;
+}
+function indicatewrong() {
+    textinput.value = textinput.value.substr(0, (textinput.value.length-1)) + texttotype.innerText.charAt(textinput.value.length-1);
+}
+
+function issamechar(text,length) {
+    if(text.charAt(length - 1) === texttotype.innerText.charAt(length - 1)) {
+        return true;
+    }
+    return false;
+}
