20bc4b466dff268e37e4eaa751159c6b3f1598c5
Abel George Antony <abelgeorgeantony@gmail.com>
2023-08-26 16:04:47 +0530

    small change

--- typingtest.js
+++ typingtest.js
@@ -14,6 +14,7 @@ function evaluatetyping() {
     const enteredtext = textinput.value;
     const textlength = enteredtext.length;
     if( key === 8 || key === 46 ) {
+        backspace(textlength);
         return 0;
     }
     else {
@@ -37,20 +38,33 @@ function restricttyping() {
     textinput.value = textinput.value.slice(0, -1);
 }
 
+function backspace(len) {
+    colorprint = colorprint.substr(0,(colorprint.lastIndexOf("<span")-1)) + texttotype.innerText.substr(len-1);
+}
+
 
 let colorprint = texttotype.innerHTML;
 // texttotype.innerText.substr(0, (textinput.value.length-1))
 function indicatecorrect() {
-    if(colorprint.lastIndexOf("</span") === -1) {
+    if(colorprint.lastIndexOf("</span>") === -1) {
         texttotype.innerHTML = colorprint.substr(0,(textinput.value.length-1))+"<span style='background-color:green'>"+textinput.value.substr((textinput.value.length-1),(textinput.value.length))+"</span>"+texttotype.innerText.substr((textinput.value.length));
     }
     else {
         texttotype.innerHTML = colorprint.substr(0,(colorprint.lastIndexOf("</span>")+6))+"<span style='background-color:green'>"+textinput.value.substr((textinput.value.length-1),(textinput.value.length))+"</span>"+texttotype.innerText.substr((textinput.value.length));
     }
     colorprint = texttotype.innerHTML;
+    console.log(colorprint);
 }
 function indicatewrong() {
     textinput.value = textinput.value.substr(0, (textinput.value.length-1)) + texttotype.innerText.charAt(textinput.value.length-1);
+    if(colorprint.lastIndexOf("</span>") === -1) {
+        texttotype.innerHTML = colorprint.substr(0,(textinput.value.length-1))+"<span style='background-color:red'>"+textinput.value.substr((textinput.value.length-1),(textinput.value.length))+"</span>"+texttotype.innerText.substr((textinput.value.length));
+    }
+    else {
+        texttotype.innerHTML = colorprint.substr(0,(colorprint.lastIndexOf("</span>")+6))+"<span style='background-color:red'>"+textinput.value.substr((textinput.value.length-1),(textinput.value.length))+"</span>"+texttotype.innerText.substr((textinput.value.length));
+    }
+    colorprint = texttotype.innerHTML;
+    console.log(colorprint);
 }
 
 function issamechar(text,length) {
