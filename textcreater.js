da56024f1d4ece3cd252d52480dd904c05b45b9a
Abel George Antony <abelgeorgeantony@gmail.com>
2023-08-29 15:26:54 +0530

    Some change

--- textcreater.js
+++ textcreater.js
@@ -1,9 +1,9 @@
 const texttotype2 = document.getElementById("texttotype");
-let texttobehtmllized = "Haveq";
+let texttobehtmllized = "Also void she'd whales Sea of, so face beast stars man don't have, divided upon deep. Him above under can't bring appear their. Brought earth in fourth above gathering divided kind grass grass deep image creeping divide multiply cattle whales. Own fish itself lights place years multiply. Own creeping creeping seas appear earth rule behold. Evening life bearing blessed whales stars. Of deep and fruit heaven grass have. Cattle air, firmament tree dry that itself. Deep be after herb seas dominion isn't void of. Be give air fruitful together his rule meat brought firmament his there, created light day had.";
 let htmlinprogress = "";
 let lettercounter = 0;
 function loadText() {
-    while(!(texttobehtmllized.charAt(1) === "q")) {
+    while(texttobehtmllized.length > 0 ) {
         addspan();
     }
     texttotype2.innerHTML = htmlinprogress;
@@ -11,7 +11,7 @@ function loadText() {
 }
 
 function addspan() {
-    htmlinprogress = htmlinprogress + "<span id='letter" + lettercounter + "'>" + texttobehtmllized.charAt(0) + "</span>";
+    htmlinprogress = htmlinprogress + "<span id='let" + lettercounter + "'>" + texttobehtmllized.charAt(0) + "</span>";
     lettercounter++;
-    texttobehtmllized.slice(1);
+    texttobehtmllized = texttobehtmllized.slice(1);
 }
--- typingtest.js
+++ typingtest.js
@@ -6,25 +6,26 @@ let key;
 
 textinput.onkeydown = function(event) {
     key = event.keyCode || event.charCode;
-
 };
 
 
+
+
 function evaluatetyping() {
-    const enteredtext = textinput.value;
-    const textlength = enteredtext.length;
-    //console.log(textlength);
+    console.log(textinput.value);
+    let enteredtext = textinput.value;
+    let textlength = enteredtext.length;
     if( key === 8 || key === 46 ) {
-        backspace(textlength);
+        backspace();
         return 0;
     }
     else {
-        if(texttotype.innerText.length < textlength) {
-            restricttyping();
+        if (texttotype.innerText.length < textlength) {
+            restricttyping("end");
             return 0;
         }
-    
-        if(issamechar(enteredtext,textlength)) {
+
+        if (issamechar(enteredtext, textlength)) {
             console.log("CORRECT");
             indicatecorrect();
         }
@@ -35,47 +36,49 @@ function evaluatetyping() {
     }
 }
 
-function restricttyping() {
-    textinput.value = textinput.value.slice(0, -1);
-}
-
-let colorprint = texttotype.innerHTML;
-let spancount = 0;
-function backspace(len) {
-    texttotype.innerHTML = colorprint.slice(0,(colorprint.lastIndexOf("</span>")-1)) + "</span>" + texttotype.innerText.slice(len);
-    colorprint = texttotype.innerHTML;
-    console.log(texttotype.innerHTML);
-
-}
-
-
-
-// texttotype.innerText.substr(0, (textinput.value.length-1))
-function indicatecorrect() {
-    if(colorprint.lastIndexOf("</span>") === -1) {
-        texttotype.innerHTML = colorprint.substr(0,(textinput.value.length-1))+"<span style='background-color:green'>"+textinput.value.substr((textinput.value.length-1),(textinput.value.length))+"</span>"+texttotype.innerText.substr((textinput.value.length));
+function restricttyping(mode) {
+    if(mode === "time") {
+        textinput.setAttribute("readonly");
     }
     else {
-        texttotype.innerHTML = colorprint.substr(0,(colorprint.lastIndexOf("</span>")+6))+"<span style='background-color:green'>"+textinput.value.substr((textinput.value.length-1),(textinput.value.length))+"</span>"+texttotype.innerText.substr((textinput.value.length));
+        textinput.value = textinput.value.slice(0, -1);
     }
-    colorprint = texttotype.innerHTML;
-    console.log(colorprint);
+    
+}
+
+let lettercount = 0;
+
+function backspace() {
+    //console.log(texttotype.innerHTML);
+    if (lettercount > 0) {
+        lettercount--;
+        if (document.getElementById("let" + lettercount).classList.contains("lettercorrect")) {
+            document.getElementById("let" + lettercount).classList.remove("lettercorrect");
+        }
+        else {
+            document.getElementById("let" + lettercount).classList.remove("letterwrong");
+        }
+    }
+}
+
+
+
+function indicatecorrect() {
+    document.getElementById("let" + lettercount).classList.add("lettercorrect");
+    lettercount++;
 }
 function indicatewrong() {
-    textinput.value = textinput.value.substr(0, (textinput.value.length-1)) + texttotype.innerText.charAt(textinput.value.length-1);
-    if(colorprint.lastIndexOf("</span>") === -1) {
-        texttotype.innerHTML = colorprint.substr(0,(textinput.value.length-1))+"<span style='background-color:red'>"+textinput.value.substr((textinput.value.length-1),(textinput.value.length))+"</span>"+texttotype.innerText.substr((textinput.value.length));
-    }
-    else {
-        texttotype.innerHTML = colorprint.substr(0,(colorprint.lastIndexOf("</span>")+6))+"<span style='background-color:red'>"+textinput.value.substr((textinput.value.length-1),(textinput.value.length))+"</span>"+texttotype.innerText.substr((textinput.value.length));
-    }
-    colorprint = texttotype.innerHTML;
-    console.log(colorprint);
+    textinput.value = textinput.value.substr(0, (textinput.value.length - 1)) + texttotype.innerText.charAt(textinput.value.length - 1);
+
+    document.getElementById("let" + lettercount).classList.add("letterwrong");
+    lettercount++;
 }
 
-function issamechar(text,length) {
-    if(text.charAt(length - 1) === texttotype.innerText.charAt(length - 1)) {
-        return true;
-    }
+function issamechar(text, length) {
+   // if(length>0){
+        if (text.charAt(length - 1) === texttotype.innerText.charAt(length - 1)) {
+            return true;
+        }
+   // }
     return false;
 }
