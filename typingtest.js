f6d383da3302871152459b5051da1521f425fe1c
Abel George Antony <abelgeorgeantony@gmail.com>
2023-08-31 18:43:48 +0530

    some changes

--- typingtest.js
+++ typingtest.js
@@ -4,8 +4,8 @@ const textinput = document.getElementById("testspace");
 let key;
 
 
-textinput.onkeydown = function(event) {
-    key = event.keyCode || event.charCode;
+textinput.onkeydown = function (event) {
+    key = event.key;
 };
 
 
@@ -13,48 +13,50 @@ textinput.onkeydown = function(event) {
 
 function evaluatetyping() {
     let enteredtext = textinput.innerText;
-    enteredtext = enteredtext.split( /\s+/ ).join( " " );
+    enteredtext = enteredtext.split(/\s+/).join(" ");
     let textlength = enteredtext.length;
-    console.log("Typed text:" + enteredtext + "\nNow typed char:" + enteredtext.charAt(textlength-1));
-    if( key === 8 || key === 46 ) {
+    console.log("Typed text:" + enteredtext + "\nNow typed char:" + enteredtext.charAt(textlength - 1));
+    if (key === "Backspace") {
         backspace();
         return 0;
     }
-    else {
-        if (texttotype.innerText.length < textlength) {
-            restricttyping();
-            return 0;
-        }
-
-        if (issamechar(enteredtext, textlength)) {
-            console.log("CORRECT");
-            indicatecorrect();
-        }
-        else {
-            console.log("INCORRECT");
-            indicatewrong();
-        }
+    else if (key === " ") {
+        console.log("Space pressed");
     }
-    var element = textinput;
-            var selected = window.getSelection();
-            var range = document.createRange();
-            range.setStart(element.childNodes[0], textinput.innerText.length);
-            range.collapse(true);
-            selected.removeAllRanges();
-            selected.addRange(range); 
-            element.focus();
+    if (texttotype.innerText.length < textlength) {
+        restricttyping();
+        return 0;
+    }
+
+    if (issamechar(enteredtext, textlength)) {
+        console.log("CORRECT");
+        indicatecorrect();
+    }
+    else {
+        console.log("INCORRECT");
+        indicatewrong();
+    }
+
+    /*let element = textinput;
+    let selected = window.getSelection();
+    let range = document.createRange();
+    range.setStart(element.childNodes[0], textinput.innerText.length);
+    range.collapse(true);
+    selected.removeAllRanges();
+    selected.addRange(range); 
+    element.focus();*/
 }
 
 function restricttyping() {
-    console.log("Inside restricttyping function");
-    textinput.setAttribute("contenteditable","false");
+    //console.log("Inside restricttyping function");
+    textinput.setAttribute("contenteditable", "false");
     textinput.innerText = textinput.innerText.slice(0, -1);
 }
 
 let lettercount = 0;
 
 function backspace() {
-    console.log("Inside backspace function");
+    //console.log("Inside backspace function");
     if (lettercount > 0) {
         lettercount--;
         if (document.getElementById("let" + lettercount).classList.contains("lettercorrect")) {
@@ -69,25 +71,25 @@ function backspace() {
 
 
 function indicatecorrect() {
-    console.log("Inside indicatecorrect function");
+    //console.log("Inside indicatecorrect function");
     document.getElementById("let" + lettercount).classList.add("lettercorrect");
     lettercount++;
 }
 function indicatewrong() {
-    console.log("Inside indicatewrong function");
-    textinput.innerText = textinput.innerText.slice(0, (textinput.innerText.length - 1)) + texttotype.innerText.charAt(textinput.innerText.length - 1);
+    //console.log("Inside indicatewrong function");
+    //textinput.innerText = textinput.innerText.slice(0, (textinput.innerText.length - 1)) + texttotype.innerText.charAt(textinput.innerText.length - 1);
     document.getElementById("let" + lettercount).classList.add("letterwrong");
     lettercount++;
 }
 
 function issamechar(text, length) {
-    console.log("Inside issamechar function");
-   // if(length>0){
+    //console.log("Inside issamechar function");
+    if (length > 0) {
         if (text.charAt(length - 1) === texttotype.innerText.charAt(length - 1)) {
             console.log("Same letter");
             return true;
         }
-   // }
-    console.log("different letter");
+    }
+    //console.log("different letter");
     return false;
 }
